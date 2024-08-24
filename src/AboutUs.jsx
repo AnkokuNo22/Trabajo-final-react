7d6eb84b57132754105d24dac794479973fc55cc
AnkokuNo22 <ankokunoshi22@gmail.com>
2024-08-24 08:52:55 -0500

    Add files via upload

---
+++ src/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+    return (
+        <div className="about-us-container">
+            {/* <h1 className="about-us-heading">About Us</h1> */}
+            <p className="about-us-description">Cultivating life, cultivating dreams.</p>
+            <p className="about-us-content">
+            In our nursery, each plant is a seed of hope that we germinate with dedication.
+            We believe that cultivating life is more than just planting and watering; it is nurturing dreams and connecting with nature.
+            With each plant we deliver, we share a little piece of our garden and invite you to
+            cultivate your own dreams, one by one, like the flowers that bloom under the sun.
+            </p>
+            {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+            <p className="about-us-content">
+            "Looking for an oasis of tranquility in your home? Visit us and be amazed by the
+            variety of plants we have for you. Cultivating life, cultivating dreams."
+            </p>
+        </div>
+    );
+}
+
+export default AboutUs;
---
+++ src/CartItem.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementQuantity, decrementQuantity, removeItem } from './CartSlice';
+import './CartItem.css';
+
+const CartItem = ({ onContinueShopping }) => {
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
+
+
+    // Calculate total amount for all products in the cart
+    const calculateTotalAmount = () => {
+
+        let totalCost = 0;
+
+        cart.forEach((item) => {
+
+            const cost = parseFloat(item.cost.replace('$', ''));;
+            const quantity = parseInt(item.quantity);
+
+            if (!isNaN(cost) && !isNaN(quantity)) {
+                totalCost += cost * quantity;
+            } else {
+                console.error('Error en el cálculo del total: cost o quantity no es un número válido.');
+            }
+        });
+
+        return totalCost
+    };
+
+    const handleContinueShopping = (e) => {
+        
+
+        if (onContinueShopping) {
+            onContinueShopping(e); //
+        }
+    };
+
+    const handleIncrement = (item) => {
+        dispatch(incrementQuantity(item));
+    };
+
+    const handleDecrement = (item) => {
+        dispatch(decrementQuantity(item));
+    };
+
+    const handleRemove = (item) => {
+        dispatch(removeItem(item));
+    };
+
+    // Calculate total cost based on quantity for an item
+    const calculateTotalCost = (item) => {
+        let totalCost = 0;
+        const cost = parseFloat(item.cost.replace('$', ''));;
+        const quantity = parseInt(item.quantity);
+
+        if (!isNaN(cost) && !isNaN(quantity)) {
+            totalCost += cost * quantity;
+        } else {
+            console.error('Error en el cálculo del total: cost o quantity no es un número válido.');
+        }
+        return totalCost
+    };
+
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
+
+    return (
+        <div className="cart-container">
+            <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+            <div>
+                {cart.map(item => (
+                    <div className="cart-item" key={item.name}>
+                        <img className="cart-item-image" src={item.image} alt={item.name} />
+                        <div className="cart-item-details">
+                            <div className="cart-item-name">{item.name}</div>
+                            <div className="cart-item-cost">{item.cost}</div>
+                            <div className="cart-item-quantity">
+                                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                                <span className="cart-item-quantity-value">{item.quantity}</span>
+                                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                            </div>
+                            <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+                            <button className="cart-item-delete" onClick={() => handleRemove(item.name)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+            <div className="continue_shopping_btn">
+                <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+                <br />
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
+            </div>
+        </div>
+    );
+};
+
+export default CartItem;
+
+
